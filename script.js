7af52b901613cd4540c1b21e2fa5e425b63cb29f
Francisco Costa <francisco100eg@gmail.com>
2022-07-06 19:20:29 -0300

    Sem barrascroll com arrows

--- script.js
+++ script.js
@@ -168,3 +168,20 @@ function resestGame() {
       gameStart();
 }
 scoreTxt.innertext = window.score;
+
+var keys = {};
+window.addEventListener("keydown",
+    function(e){
+        keys[e.code] = true;
+        switch(e.code){
+            case "ArrowUp": case "ArrowDown": case "ArrowLeft": case "ArrowRight":
+            case "Space": e.preventDefault(); break;
+            default: break; // do not block other keys
+        }
+    },
+false);
+window.addEventListener('keyup',
+    function(e){
+        keys[e.code] = false;
+    },
+false);
